9c536f58ecade513d7728dbd0dadaa608ea317c0
Sudha Raamakrishnan <sudhara@users.noreply.github.com>
2024-10-21 15:04:12 -0700

    Update test.py

--- epa/test.py
+++ epa/test.py
@@ -1,16 +1,10 @@
 import streamlit as st
-from streamlit_navigation_bar import st_navbar
 
-style = {
-"appview-container" : {"padding-top": "100px",},
-"main" : {"padding-top": "100px",},
-"block-container" : {"padding-top": "100px",}
-}
+def home():
+        print ("hello")
 
-page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"], styles=style)
-
-if page == "Home":
-        st.write("Home")
-elif page == "Documentation":
-        st.write("Documentation")
-st.write(page)
+def document():
+        print ("document")
+        
+pg = st.navigation([st.Page("home"), st.Page("document")])
+pg.run()
