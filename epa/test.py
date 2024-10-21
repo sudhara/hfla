96afb5d6536cc85b71fe29b81c9fe00e126df5af
Sudha Raamakrishnan <sudhara@users.noreply.github.com>
2024-10-21 15:08:05 -0700

    Update test.py

--- epa/test.py
+++ epa/test.py
@@ -1,10 +1,29 @@
 import streamlit as st
 
-def home():
-        print ("hello")
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
-def document():
-        print ("document")
-        
-pg = st.navigation([st.Page("home()"), st.Page("document()")])
+def login():
+    if st.button("Log in"):
+        st.session_state.logged_in = True
+        st.rerun()
+
+def logout():
+    if st.button("Log out"):
+        st.session_state.logged_in = False
+        st.rerun()
+
+login_page = st.Page(login, title="Log in", icon=":material/login:")
+logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+
+
+if st.session_state.logged_in:
+    pg = st.navigation(
+        {
+            "Account": [logout_page],
+        }
+    )
+else:
+    pg = st.navigation([login_page])
+
 pg.run()
