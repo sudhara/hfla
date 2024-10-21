019611faa70285b72bcb1660611f9e3cc0f1efa8
Sudha Raamakrishnan <sudhara@users.noreply.github.com>
2024-10-21 14:41:05 -0700

    Update test.py

--- epa/test.py
+++ epa/test.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
+<style>
+.appview-container .main .block-container{{
+        padding-top: {padding_top}rem;    }}
+</style>
+
 page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
 st.write(page)
